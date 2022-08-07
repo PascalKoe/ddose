0d06fd564a54471acb2186494cc8993440cba4a8
Pascal Koestler <pascal.koestler@outlook.de>
2022-08-08 00:19:24 +0200

    Simplify creation of CAN IDs in ISOTP examples

--- ddose-isotp/examples/receive.rs
+++ ddose-isotp/examples/receive.rs
@@ -3,8 +3,8 @@ use ddose_common::CanInterface;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let can_if = CanInterface::try_from("vcan0")?;
-    let rx_id = embedded_hal::can::Id::Standard(embedded_hal::can::StandardId::new(0x100).unwrap());
-    let tx_id = embedded_hal::can::Id::Standard(embedded_hal::can::StandardId::new(0x101).unwrap());
+    let rx_id = embedded_hal::can::StandardId::new(0x100).unwrap();
+    let tx_id = embedded_hal::can::StandardId::new(0x101).unwrap();
 
     let mut isotp_connection = ddose_isotp::IsotpConnection::open(&can_if, tx_id, rx_id)?;
 
